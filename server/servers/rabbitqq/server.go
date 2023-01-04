50a12c4fedba626cb17cdafd31831e737efd924a
anhmuz <32794622+anhmuz@users.noreply.github.com>
2023-01-04 15:30:03 +0200

    context propagation in server and service (#13)

--- server/servers/rabbitqq/server.go
+++ server/servers/rabbitqq/server.go
@@ -8,7 +8,6 @@ import (
 	"qq/pkg/rabbitqq"
 	"qq/services/qq"
 	"sync"
-	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -91,7 +90,8 @@ func (s server) Serve() error {
 			defer wg.Done()
 
 			for msg := range msgs {
-				err = s.handleRawMessage(msg.Body, msg.CorrelationId, msg.ReplyTo)
+				ctx := context.Background()
+				err = s.handleRawMessage(ctx, msg.Body, msg.CorrelationId, msg.ReplyTo)
 				if err != nil {
 					log.Println(fmt.Errorf("failed to handle message: %w", err))
 				}
@@ -104,7 +104,7 @@ func (s server) Serve() error {
 	return nil
 }
 
-func handleMessage[Message any, ReplyMessage any](s server, body []byte, corrId string, replyTo string, proc func(message Message) ReplyMessage) error {
+func handleMessage[Message any, ReplyMessage any](ctx context.Context, s server, body []byte, corrId string, replyTo string, proc func(message Message) ReplyMessage) error {
 	var message Message
 	err := json.Unmarshal(body, &message)
 	if err != nil {
@@ -112,9 +112,6 @@ func handleMessage[Message any, ReplyMessage any](s server, body []byte, corrId 
 	}
 
 	replyMessage := proc(message)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
 
 	jsonReplyMessage, err := json.Marshal(replyMessage)
 	if err != nil {
@@ -138,7 +135,7 @@ func handleMessage[Message any, ReplyMessage any](s server, body []byte, corrId 
 	return nil
 }
 
-func (s server) handleRawMessage(body []byte, corrId string, replyTo string) error {
+func (s server) handleRawMessage(ctx context.Context, body []byte, corrId string, replyTo string) error {
 	var baseMessage rabbitqq.BaseMessage
 	err := json.Unmarshal(body, &baseMessage)
 	if err != nil {
@@ -147,39 +144,39 @@ func (s server) handleRawMessage(body []byte, corrId string, replyTo string) err
 
 	switch baseMessage.Name {
 	case "add":
-		err = handleMessage(s, body, corrId, replyTo,
+		err = handleMessage(ctx, s, body, corrId, replyTo,
 			func(addMessage rabbitqq.AddMessage) rabbitqq.AddReplyMessage {
 				entity := FromAddMessage(addMessage)
-				return ToAddReplyMessage(s.service.Add(entity))
+				return ToAddReplyMessage(s.service.Add(ctx, entity))
 			})
 		if err != nil {
 			return fmt.Errorf("failed to handle add message: %w", err)
 		}
 
 	case "remove":
-		err = handleMessage(s, body, corrId, replyTo,
+		err = handleMessage(ctx, s, body, corrId, replyTo,
 			func(removeMessage rabbitqq.RemoveMessage) rabbitqq.RemoveReplyMessage {
 				key := FromRemoveMessage(removeMessage)
-				return ToRemoveReplyMessage(s.service.Remove(key))
+				return ToRemoveReplyMessage(s.service.Remove(ctx, key))
 			})
 		if err != nil {
 			return fmt.Errorf("failed to handle remove message: %w", err)
 		}
 
 	case "get":
-		err = handleMessage(s, body, corrId, replyTo,
+		err = handleMessage(ctx, s, body, corrId, replyTo,
 			func(getMessage rabbitqq.GetMessage) rabbitqq.GetReplyMessage {
 				key := FromGetMessage(getMessage)
-				return ToGetReplyMessage(s.service.Get(key))
+				return ToGetReplyMessage(s.service.Get(ctx, key))
 			})
 		if err != nil {
 			return fmt.Errorf("failed to handle get message: %w", err)
 		}
 
 	case "get all":
-		err = handleMessage(s, body, corrId, replyTo,
+		err = handleMessage(ctx, s, body, corrId, replyTo,
 			func(getAllMessage rabbitqq.GetAllMessage) rabbitqq.GetAllReplyMessage {
-				return ToGetAllReplyMessage(s.service.GetAll())
+				return ToGetAllReplyMessage(s.service.GetAll(ctx))
 			})
 		if err != nil {
 			return fmt.Errorf("failed to handle add message: %w", err)
--- services/qq/service.go
+++ services/qq/service.go
@@ -1,16 +1,17 @@
 package qq
 
 import (
+	"context"
 	"log"
 	"qq/models"
 	"qq/repos/qq"
 )
 
 type Service interface {
-	Add(entity models.Entity) bool
-	Remove(key string) bool
-	Get(key string) *models.Entity
-	GetAll() []models.Entity
+	Add(ctx context.Context, entity models.Entity) bool
+	Remove(ctx context.Context, key string) bool
+	Get(ctx context.Context, key string) *models.Entity
+	GetAll(ctx context.Context) []models.Entity
 }
 
 type service struct {
@@ -25,22 +26,22 @@ func NewService(database qq.Database) (Service, error) {
 	}, nil
 }
 
-func (s service) Add(entity models.Entity) bool {
+func (s service) Add(ctx context.Context, entity models.Entity) bool {
 	log.Printf("service: add %+v\n", entity)
 	return s.database.Add(entity)
 }
 
-func (s service) Remove(key string) bool {
+func (s service) Remove(ctx context.Context, key string) bool {
 	log.Printf("service: remove %s\n", key)
 	return s.database.Remove(key)
 }
 
-func (s service) Get(key string) *models.Entity {
+func (s service) Get(ctx context.Context, key string) *models.Entity {
 	log.Printf("service: get %s\n", key)
 	return s.database.Get(key)
 }
 
-func (s service) GetAll() []models.Entity {
+func (s service) GetAll(ctx context.Context) []models.Entity {
 	log.Println("service: get all")
 	return s.database.GetAll()
 }
