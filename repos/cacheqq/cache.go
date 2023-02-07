040d45cf86d3fe233abb6050e56f05bd9468e565
anhmuz <anhelina.muzychenko@gmail.com>
2023-02-07 14:27:50 +0200

    last fix

--- repos/cacheqq/cache.go
+++ repos/cacheqq/cache.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 	"qq/models"
 	"qq/pkg/rabbitqq"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
 
 type Cache interface {
 	GetEntity(ctx context.Context, key string) (*models.Entity, error)
-	SetEntity(ctx context.Context, entity *models.Entity) error
+	SetEntity(ctx context.Context, key string, entity *models.Entity) error
+	DeleteEntity(ctx context.Context, key string) error
 }
 
 type cache struct {
@@ -36,19 +38,41 @@ func (c cache) GetEntity(ctx context.Context, key string) (*models.Entity, error
 	value, err := c.redisClient.Get(ctx, key).Result()
 
 	if err == redis.Nil {
-		return nil, nil
+		return nil, fmt.Errorf("key %s does not exist", key)
+
 	}
 	if err != nil {
 		return nil, fmt.Errorf("failed to get key %s: %w", key, err)
 	}
 
+	if value == "" {
+		return nil, nil
+	}
+
 	return &models.Entity{Key: key, Value: value}, nil
 }
 
-func (c cache) SetEntity(ctx context.Context, entity *models.Entity) error {
-	err := c.redisClient.Set(ctx, entity.Key, entity.Value, 0).Err()
+func (c cache) SetEntity(ctx context.Context, key string, entity *models.Entity) error {
+	var value string
+
+	if entity != nil {
+		value = entity.Value
+	}
+
+	err := c.redisClient.Set(ctx, key, value, 10*time.Minute).Err()
+
 	if err != nil {
-		return fmt.Errorf("failed to set key %s, value %s: %w", entity.Key, entity.Value, err)
+		return fmt.Errorf("failed to set key %s, value %s", key, value)
+	}
+
+	return nil
+}
+
+func (c cache) DeleteEntity(ctx context.Context, key string) error {
+	err := c.redisClient.Del(ctx, key).Err()
+
+	if err != nil {
+		return fmt.Errorf("failed to delete key %s", key)
 	}
 
 	return nil
--- services/qq/service.go
+++ services/qq/service.go
@@ -31,11 +31,34 @@ func NewService(database qq.Database, cache cacheqq.Cache) (Service, error) {
 
 func (s service) Add(ctx context.Context, entity models.Entity) bool {
 	log.Debug(ctx, "service: add", log.Args{"entity": entity})
-	return s.database.Add(entity)
+
+	s.database.Add(entity)
+
+	cachedEntity, err := s.cache.GetEntity(ctx, entity.Key)
+	_ = cachedEntity
+
+	if err != nil {
+		return true
+	}
+
+	err = s.cache.SetEntity(ctx, entity.Key, &entity)
+	if err == nil {
+		log.Debug(ctx, "set to cache", log.Args{"key": entity.Key, "entity": entity})
+	} else {
+		log.Warning(ctx, "failed to set to cache", log.Args{"error": err})
+	}
+
+	return true
 }
 
 func (s service) Remove(ctx context.Context, key string) bool {
 	log.Debug(ctx, "service: remove", log.Args{"key": key})
+
+	err := s.cache.DeleteEntity(ctx, key)
+	if err != nil {
+		log.Warning(ctx, "failed to delete from cache", log.Args{"error": err})
+	}
+
 	return s.database.Remove(key)
 }
 
@@ -44,28 +67,19 @@ func (s service) Get(ctx context.Context, key string) *models.Entity {
 
 	entity, err := s.cache.GetEntity(ctx, key)
 
-	if entity != nil {
-		log.Debug(ctx, "service: get from cache", log.Args{"key": key})
-		return entity
-	}
+	log.Debug(ctx, "get from cache", log.Args{"key": key, "entity": entity, "error": err})
 
 	if err == nil {
-		log.Warning(ctx, "service: key does not exist in cache", log.Args{"key": key})
-	} else {
-		log.Error(ctx, "service: failed to get from cache", log.Args{"error": err})
+		return entity
 	}
 
 	entity = s.database.Get(key)
 
-	if entity == nil {
-		return nil
-	}
-
-	err = s.cache.SetEntity(ctx, entity)
+	err = s.cache.SetEntity(ctx, key, entity)
 	if err == nil {
-		log.Debug(ctx, "service: set to cache", log.Args{"entity": entity})
+		log.Debug(ctx, "set to cache", log.Args{"key": key, "entity": entity})
 	} else {
-		log.Error(ctx, "service: failed to set to cache", log.Args{"error": err})
+		log.Warning(ctx, "failed to set to cache", log.Args{"error": err})
 	}
 
 	return entity
